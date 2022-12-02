d457fefe195d3524ca1b9a44884ecc20a2afcf4d
wldlsgur <dhgg321@naver.com>
2022-12-02 19:19:42 +0900

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
+import "./components/common/NotFound";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/common/NotFound.js
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <>404 Error</>;
+};
+
+export default NotFound;
