a8113771238f007a090cb98d825a76b5df8731aa
5469804 <92029126+5469804@users.noreply.github.com>
2022-12-21 20:03:44 +0900

    문답페이지 및 글리치,암전 추가
    -

--- client/src/components/qna/Answer.js
+++ client/src/components/qna/Answer.js
@@ -3,13 +3,13 @@ import QnaImg from "./QnaImg";
 import Footer from "../common/footer";
 import { useState } from 'react';
 
-function Answer( {item, path, indexAdd, controlView} ) {
+function Answer( {index, item, path, indexAdd, controlView} ) {
     const [tidx, setTidx] = useState(0);
 
     const who = item[path].who[tidx];
     const content =  item[path].content[tidx];
     const imgsrc =  item[path].talkimg[tidx];
-
+    
     const contentClick = () =>{
         if( item[path].who.length === tidx+1){
             indexAdd();
@@ -19,7 +19,26 @@ function Answer( {item, path, indexAdd, controlView} ) {
             setTidx(tidx+1)
         }
     }
-    
+    if(index === 3 && tidx === 0){
+        return(
+            <GlitchD onAnimationEnd={contentClick}></GlitchD>
+        );
+    }
+
+    if(index === 1 && tidx === 1){
+        return(
+            <AnimationD onClick={contentClick}>
+                <QnaImg url={imgsrc}></QnaImg>
+                <ADiv>
+                    <AWho>{who}</AWho>
+                    <AContentDiv onClick={contentClick}>
+                        <AContentP>{content}</AContentP>
+                    </AContentDiv>
+                </ADiv>
+                <Footer></Footer>
+            </AnimationD>
+        );
+    }
     return(
         <>
             <QnaImg url={imgsrc}></QnaImg>
@@ -34,7 +53,45 @@ function Answer( {item, path, indexAdd, controlView} ) {
     );
 }
 
+const GlitchD = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 
+  background-image: url("/assets/Qnaimges/glitch.gif");
+  background-size: 100% 100%;
+  overflow: hidden;
+  
+  animation-name: glitch;
+  animation-duration: 2s;
+
+  @keyframes glitch {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 1;
+    }
+`;
+
+const AnimationD = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  animation-name: fadeout;
+  animation-duration: 10s;
+
+  @keyframes fadeout {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+`;
 const ADiv = styled.div`
   witdh: 100%;
   flex: 1;
