cbe3a69542a32d2328c329dba9a14ca30448768a
5469804 <92029126+5469804@users.noreply.github.com>
2022-12-26 18:58:23 +0900

    질문 a,b,c 한글자씩 나오게 수정

--- client/src/components/qna/Question.js
+++ client/src/components/qna/Question.js
@@ -1,8 +1,66 @@
+import { useEffect, useRef } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 function Qusetion( {item, pathSet} ) {
     const question = item.question;
-    const answer = item.answer;
+    const answerA = item.answer[0];
+    const answerB = item.answer[1];
+    const answerC = item.answer[2];
+
+    const [aAnswer, setaAnswer] = useState('');
+    const [bAnswer, setbAnswer] = useState('');
+    const [cAnswer, setcAnswer] = useState('');
+
+    const [acount, setaCount] = useState(0);
+    const [bcount, setbCount] = useState(0);
+    const [ccount, setcCount] = useState(0);
+
+    const unmounted = useRef(false);
+    useEffect(() => {
+      Math.max(answerA.length, answerB.length, answerC.length)
+      const typingInterval = setInterval(() => {
+        setaAnswer((value) => {
+          let resulta = value ? value + answerA[acount] : answerA[0];
+          setaCount(acount + 1);
+  
+          if (acount >= answerA.length) {
+            setaCount(acount);
+            setaAnswer(aAnswer);
+            
+          }
+  
+          return resulta;
+        });
+        setbAnswer((value) => {
+          let resultb = value ? value + answerB[bcount] : answerB[0];
+          setbCount(bcount + 1);
+  
+          if (bcount >= answerB.length) {
+            setbCount(bcount);
+            setbAnswer(bAnswer);
+          }
+  
+          return resultb;
+        });
+        setcAnswer((value) => {
+          let resultc = value ? value + answerC[ccount] : answerC[0];
+          setcCount(ccount + 1);
+  
+          if (ccount >= answerC.length) {
+            setcCount(ccount);
+            setcAnswer(cAnswer);
+          }
+  
+          return resultc;
+        });
+      }, 120);
+
+      return () => {
+        clearInterval(typingInterval);
+      };
+    });
+
     const questionClick = (idx)=>{
         pathSet(idx)
     }
@@ -16,15 +74,19 @@ function Qusetion( {item, pathSet} ) {
             <div>
                 <QTitle>{question}</QTitle>    
             </div>
-            {
-                answer.map((item, idx) => 
-                    <div key={idx}><QContentP onClick={() => questionClick(idx)}>{item}</QContentP></div>
-                )
-            }
+            <QContentP onClick={() => questionClick(0)}>{aAnswer}</QContentP>
+            <QContentP onClick={() => questionClick(1)}>{bAnswer}</QContentP>
+            <QContentP onClick={() => questionClick(2)}>{cAnswer}</QContentP>
         </QDiv>
     );
 }
 
+
+// {
+//   answer.map((item, idx) => 
+//       <div key={idx}><QContentP onClick={() => questionClick(idx)}>{item}</QContentP></div>
+//   )
+// }
 const QDiv = styled.div`
   witdh: 100%;
   flex: 1 1 55%;
@@ -32,24 +94,24 @@ const QDiv = styled.div`
   background-image: url("/assets/Qnaimges/question.png");
   background-size: 100% 100%;
   overflow: hidden;
-  padding : 4% 6% 0 6%;
+  padding : 8% 6% 0 6%;
   
   display: grid;
   grid-template-columns: 7% 90%;
-  grid-template-rows: 30% 13% 18% 18%; 
+  grid-template-rows: 30% 18% 18% 18%; 
   grid-auto-flow : column;
   grid-column-gap : 2%;
   grid-row-gap: 5%;
   
-  @media screen and (min-height: 770px) {
-    @media screen and (min-width: 850px){
-        padding : 5% 6% 0 5%;
+  @media screen and (min-height: 800px) {
+    @media screen and (min-width: 680px){
+        padding : 5% 5% 0 5%;
     }
-    padding : 10% 6% 0 5%;
+    padding : 13% 5% 0 5%;
   }
   
   @media screen and (min-width: 950px) {
-    padding : 2% 6% 0 5%;
+    padding : 2% 5% 0 5%;
   }
 `;
 
@@ -74,7 +136,5 @@ const QContentP = styled.p`
   font-size: 17px;
   line-height: 22px;
   padding: 0 7% 0 0;
-
-  
 `;
 export default Qusetion;
