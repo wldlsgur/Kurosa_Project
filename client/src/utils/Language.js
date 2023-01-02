1f4c4449fc69dce6177246e263fa7396fe5ebe2b
5469804 <92029126+5469804@users.noreply.github.com>
2023-01-02 18:06:43 +0900

    제작진 페이지 일본어 번역 추가

--- client/src/utils/Language.js
+++ client/src/utils/Language.js
@@ -6,17 +6,21 @@ import ResultDataKor from "./ResultDataKor";
 import ResultDataJp from "./ResultDataJp";
 import QnaDataKor from "./QnaDataKor";
 import QnaDataJp from "./QnaDataJp";
+import ProducersKor from "./ProducersDataKor";
+import ProducersJp from "./ProducersDataJp";
 
 const kor = {
   warring: WarringDataKor,
   talk: talkDataKr,
   result: ResultDataKor,
   qna: QnaDataKor,
+  producers: ProducersKor,
 };
 const jp = {
   warring: WarringDataJp,
   talk: talkDataJp,
   result: ResultDataJp,
   qna: QnaDataJp,
+  producers: ProducersJp,
 };
 export { kor, jp };
---
+++ client/src/utils/ProducersDataJp.js
@@ -0,0 +1,56 @@
+const pData = [
+  {
+    title: "DIRECTOR",
+    list : ["TEIMI"],
+  },
+  {
+    title: "DESIGN",
+    list : [
+      "CHARACTER - TEIMI", 
+      "LOGO - TEIMI", 
+      "3D - TEIMI", 
+      "LUCKY ITEM - Kim Yunji", 
+      "FRAME - Kim Yunji", 
+      "POSTER - TEIMI, Kim Yunji",
+      "SNS POST - TEIMI, Kim Yunji",
+    ],
+  },
+  {
+    title: "PROGRAMMER",
+    list : ["Ji Inhyeok, Lee Seung Hyun"],
+  },
+  {
+    title: "SOUND DESIGN",
+    list : ["WAv2, Park Seung joo"],
+  },
+  {
+    title: "WRITING",
+    list : [
+      "TEIMI",
+      "LUCKY ITEM - TEIMI, Kim Yunji",
+    ],
+  },
+  {
+    title: "TRANSLATION",
+    list : ["MO"],
+  },
+  {
+    title: "THANKS TO",
+    list : [
+      "KIMSOOYA",
+      "AKI",
+      "C",
+    ],
+  },
+  {
+    title: "RESOURCES",
+    list : [
+      "Everyday Cinematic Sounds",
+      "Debrup Travel & Films",
+      "FLATICON",
+      "PIXABAY",
+    ],
+  },
+];
+
+export default pData;
