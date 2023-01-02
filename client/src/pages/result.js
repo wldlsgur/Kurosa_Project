76a13260ae7e4767d17481154f6ea62350145b08
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-01-02 21:04:14 +0900

    공유 모달창 완성 및 클립보드 복사기능 완료

--- client/src/pages/result.js
+++ client/src/pages/result.js
@@ -5,15 +5,45 @@ import Footer from "../components/common/footer";
 import Item from "../components/result/item";
 import ResultMsg from "../components/result/resultMsg";
 import SnsNav from "../components/result/snsLav";
-import resultMsgImageLanguageCheck from "../utils/resultMsgImageLanguageCheck";
 import LogoImg from "../components/result/logoImg";
+import ShareImg from "../components/result/shareImg";
+import ShareModal from "../components/common/shareModal";
+
+import resultMsgImageLanguageCheck from "../utils/resultMsgImageLanguageCheck";
+import { useSelector, useDispatch } from "react-redux";
+import shareToggle from "../store/actions/result/share";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
 const ResultPage = () => {
+  const shareController = useSelector((state) => state.shareController);
+  const dispatch = useDispatch();
+  const { t, i18n } = useTranslation();
+
+  const openShareModal = () => {
+    dispatch(shareToggle(true));
+  };
+  const closeShareModal = () => {
+    dispatch(shareToggle(false));
+  };
+  const shareImg =
+    i18n.language === "kr"
+      ? "/assets/Images/share.png"
+      : "/assets/Images/shareJp.png";
+
   return (
     <WrapDiv url={"/assets/Images/talkBackground.gif"}>
       <Header></Header>
       <Item></Item>
-      <ResultMsg msgUrl={resultMsgImageLanguageCheck()}></ResultMsg>
-      <SnsNav></SnsNav>
+      <ShareImg openShareModal={openShareModal} shareImg={shareImg}></ShareImg>
+      {!shareController.isShare ? (
+        <React.Fragment>
+          <ResultMsg msgUrl={resultMsgImageLanguageCheck()}></ResultMsg>
+          <SnsNav></SnsNav>
+        </React.Fragment>
+      ) : (
+        <ShareModal closeShareModal={closeShareModal}></ShareModal>
+      )}
       <LogoImg></LogoImg>
       <Footer></Footer>
     </WrapDiv>
