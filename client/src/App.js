896e6bff0db19e68e0f95b17d2a6dfb197a9928b
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-01-06 22:54:20 +0900

    메타태그 헬멧사용

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 import rootReducer from "./store/RootReducer";
+import { Helmet } from "react-helmet-async";
 
 import Main from "./pages/main";
 import Talk from "./pages/talk";
@@ -14,6 +15,7 @@ const store = createStore(rootReducer);
 function App() {
   return (
     <div className="App">
+      <MetaTag></MetaTag>
       <BrowserRouter>
         <Provider store={store}>
           <Routes>
@@ -30,4 +32,28 @@ function App() {
   );
 }
 
+const MetaTag = () => {
+  return (
+    <Helmet>
+      <meta name="twitter:title" content="사이버 점집 KUROSA_2023" />
+      <meta name="twitter:description" content="cyber fortune teller" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="http://3.36.50.113:3000/" />
+      <meta name="twitter:image" content="https://ifh.cc/g/cvd79x.jpg" />
+      <meta
+        name="title"
+        property="og:title"
+        content="사이버 점집 KUROSA_2023"
+      />
+      <meta
+        name="description"
+        property="og:description"
+        content="cyber fortune teller"
+      />
+      <meta name="image" property="og:image" content="/favicon.ico" />
+      <meta name="url" property="og:url" content="http://3.36.50.113:3000/" />
+      <meta property="og:locale" content="ko_KR" />
+    </Helmet>
+  );
+};
 export default App;
