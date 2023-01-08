28058ecfd4dee3877e9ffa4c865d0219fabf2086
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-01-08 19:52:01 +0900

    asdf

--- client/src/Api/shareTwitter.js
+++ client/src/Api/shareTwitter.js
@@ -1,6 +1,8 @@
+import env from "../utils/env";
+
 const shareToTwitter = () => {
   var sendText = "사이버 점집 KUROSA_2023\ncyber fortune teller\n"; // 전달할 텍스트
-  var sendUrl = process.env.REACT_APP_SERVER_IP_ADRESS; // 전달할 URL
+  var sendUrl = env.serverAddress; // 전달할 URL
   window.open(
     "https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl
   );
---
+++ client/src/utils/env.js
@@ -0,0 +1,6 @@
+const env = {
+  javascriptKey: "198d199f444cfc8faa03b88d933e9fb8",
+  serverAddress: "http://3.36.50.113:3000/",
+};
+
+export default env;
