efeb9e9f0dd356b66050db843d29ceb34fa31d9d
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2023-01-18 00:47:27 +0900

    Merge branch 'main' of https://github.com/wldlsgur/kurosa

--- client/src/Api/shareTwitter.js
+++ client/src/Api/shareTwitter.js
@@ -2,7 +2,8 @@ import env from "../utils/env";
 
 const shareToTwitter = () => {
   var sendText = "사이버 점집 KUROSA_2023 cyber fortune teller"; // 전달할 텍스트
-  var sendUrl = env.serverAddress; // 전달할 URL
+  var sendUrl =
+    "https://www.instagram.com/s/aGlnaGxpZ2h0OjE4MjYxMzgzMjMzMTkwMzcw?igshid=MDJmNzVkMjY=";
   window.open(
     "https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl
   );
